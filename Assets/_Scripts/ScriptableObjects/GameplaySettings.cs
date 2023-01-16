89f0e590d561335ca8228e221103eb880055fb63
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-17 00:43:52 +0300

    more GameplaySettings!

--- Assets/_Scripts/ScriptableObjects/GameplaySettings.cs
+++ Assets/_Scripts/ScriptableObjects/GameplaySettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace Assets._Scripts.ScriptableObjects
     [CreateAssetMenu(fileName = "GameplaySettings", menuName = "ScriptableObjects/GameplaySettings", order = 0)]
     public class GameplaySettings : ScriptableObject
     {
-        public float CharacterSpeed;
+        public float CharacterSpeed = 5;
+
+        public float TileSpeed = 5;
+
+        public float SpawnX = 4f;
+
+        public float YoyoDuration = 2f;
+        public Ease Ease = Ease.Linear;
     }
 }
