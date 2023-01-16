c6e5b64a6a639ac2d400a592fadac4e89c529c99
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-17 00:43:28 +0300

    tile controller and spawner logics

---
+++ Assets/_Scripts/TileController.cs
@@ -0,0 +1,98 @@
+﻿using Assets._Scripts.Managers;
+using Assets._Scripts.ScriptableObjects;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts
+{
+    public class TileController : MonoBehaviour
+    {
+        private Tween m_Tween;
+        private TileController m_PreviousTile;
+
+        public Action TilePlaced;
+
+
+        public GameplaySettings Settings => GameManager.Instance.GameplaySettings;
+
+        private void Update()
+        {
+            if (GameManager.Instance.GameState != GameState.Playing ||
+                m_Tween == null) return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Place();
+                return;
+            }
+        }
+
+        public void Init(TileController previousTile)
+        {
+            m_PreviousTile = previousTile;
+
+            var x = Settings.SpawnX * -1;
+            m_Tween = transform.DOMoveX(x, Settings.YoyoDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Settings.Ease);
+        }
+
+        private void Place()
+        {
+            var splitDiff = transform.position.x - m_PreviousTile.transform.position.x;
+            if (Math.Abs(splitDiff) >= m_PreviousTile.transform.localScale.x)
+            {
+                Destroy(gameObject);
+                GameManager.Instance.GameState = GameState.GameOver;
+                //TODO: gameover
+            }
+            else
+            {
+                m_Tween?.Kill();
+                m_Tween = null;
+
+                Split(splitDiff);
+
+                TilePlaced?.Invoke();
+            }
+        }
+
+        private void Split(float diff)
+        {
+            var newSize = m_PreviousTile.transform.localScale.x - Mathf.Abs(diff);
+            var direction = diff > 0 ? 1 : -1;
+            var fallingTileSize = transform.localScale.x - newSize;
+
+            var newPos = m_PreviousTile.transform.position.x + (diff / 2f);
+
+            var scale = transform.localScale;
+            scale.x = newSize;
+            transform.localScale = scale;
+
+            var pos = transform.position;
+            pos.x = newPos;
+            transform.position = pos;
+
+            var edge = transform.position.x + (newSize / 2f * direction);
+            var fallingPos = edge + (fallingTileSize / 2f * direction);
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.SetParent(GameManager.Instance.CurrentLevel.transform);
+
+            scale = transform.localScale;
+            scale.x = fallingTileSize;
+            cube.transform.localScale = scale;
+
+            pos = transform.position;
+            pos.x = fallingPos;
+            cube.transform.position = pos;
+
+            cube.AddComponent<Rigidbody>();
+            Destroy(cube.gameObject, 2f);
+        }
+    }
+}
---
+++ Assets/_Scripts/TileSpawner.cs
@@ -0,0 +1,75 @@
+﻿using Assets._Scripts.Managers;
+using Assets._Scripts.ScriptableObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts
+{
+    public class TileSpawner : MonoBehaviour
+    {
+        private float m_CurrentDistance = 0f;
+        private TileController m_CurrentTile;
+        private MaterialLibrary m_MaterialLibrary;
+
+        public float TileLenght = 2;
+        public TileController TilePrefab;
+        public TileController InitialTile;
+        
+        private void Start()
+        {
+            m_CurrentDistance += TileLenght;
+            GameManager.Instance.GameStateChanged += OnStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            if (GameManager.Instance)
+            {
+                GameManager.Instance.GameStateChanged -= OnStateChanged;
+            }
+        }
+
+        public void Spawn()
+        {
+            if (m_CurrentTile == null)
+            {
+                m_CurrentTile = InitialTile;
+            }
+
+            var tile = Instantiate(TilePrefab, GameManager.Instance.CurrentLevel.transform);
+            tile.transform.localScale = m_CurrentTile.transform.localScale;
+            var direction = m_CurrentDistance % (TileLenght * 2) == 0 ? 1 : -1;
+            
+            var pos = tile.transform.position;
+            pos.x = tile.transform.localScale.x * direction;
+            pos.z = m_CurrentDistance;
+            tile.transform.position = pos;
+
+            tile.Init(m_CurrentTile);
+
+            m_CurrentTile = tile;
+            m_CurrentTile.TilePlaced += OnTilePlaced;
+
+            m_CurrentDistance += TileLenght;
+        }
+
+        private void OnStateChanged(GameState state)
+        {
+            if (state == GameState.Playing)
+            {
+                GameManager.Instance.GameStateChanged -= OnStateChanged;
+                Spawn();
+            }
+        }
+
+        private void OnTilePlaced()
+        {
+            m_CurrentTile.TilePlaced -= OnTilePlaced;
+            Spawn();
+        }
+    }
+}
