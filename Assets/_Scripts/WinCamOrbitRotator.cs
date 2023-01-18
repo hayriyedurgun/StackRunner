7527a8816b16cbe3a8ecd68f540ba4a05026ccad
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-18 22:20:48 +0300

    win cam fix

--- Assets/_Scripts/WinCamOrbitRotator.cs
+++ Assets/_Scripts/WinCamOrbitRotator.cs
@@ -1,5 +1,6 @@
 ﻿using Assets._Scripts.Managers;
 using Assets._Scripts.ScriptableObjects;
+using Cinemachine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,27 @@ namespace Assets._Scripts
 {
     public class WinCamOrbitRotator : MonoBehaviour
     {
+        private float m_Alpha;
+        private CinemachineTransposer m_Transposer;
+
         public GameplaySettings Settings => GameManager.Instance.GameplaySettings;
+
+        private void Start()
+        {
+            m_Transposer = CameraManager.Instance.WinCam.GetCinemachineComponent<CinemachineTransposer>();
+        }
 
         private void Update()
         {
-            transform.RotateAround(GameManager.Instance.Character.transform.position, Vector3.up, Settings.CamRotationSpeed
-             * Time.deltaTime);
+            var z = Mathf.Sin(m_Alpha) * Settings.Radius;
+            var x = Mathf.Cos(m_Alpha) * Settings.Radius;
+
+            var offset = m_Transposer.m_FollowOffset;
+            offset.x = x;
+            offset.z = z;
+            m_Transposer.m_FollowOffset = offset;
+
+            m_Alpha += Settings.IncrementAlpha;
         }
     }
 }
