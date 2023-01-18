3ca3caeef0c9b595e9a68fa2389628fc853ebbb6
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-19 00:35:44 +0300

    more readable and juicy canvas textes

--- Assets/_Scripts/UI/SuccessPanel.cs
+++ Assets/_Scripts/UI/SuccessPanel.cs
@@ -1,17 +1,25 @@
 ﻿using Assets._Scripts.Managers;
+using DG.Tweening;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 
 namespace Assets._Scripts.UI
 {
     public class SuccessPanel : BasePanel
     {
+        public TextMeshProUGUI Text;
+
+        private void Start()
+        {
+            Text.transform.DOScale(1.25f, .5f).SetLoops(-1, LoopType.Yoyo);
+        }
+
         public override void OnClick()
         {
-            //TODO!!!
             GameManager.Instance.GameState = GameState.Playing;
             GameManager.Instance.ProgressLevel();
         }
