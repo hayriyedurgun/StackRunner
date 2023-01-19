ce668f8b55523f7a4dbfbb4d00c2b115dfa27b9b
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-19 23:17:42 +0300

    cam rotation fix

---
+++ Assets/AimOffsetSetter.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimOffsetSetter : MonoBehaviour
+{
+    private CinemachineVirtualCamera m_Cam;
+    private CinemachineComposer m_Composer;
+
+    private void Awake()
+    {
+        m_Cam = GetComponent<CinemachineVirtualCamera>();
+        m_Composer = m_Cam.GetCinemachineComponent<CinemachineComposer>();
+    }
+
+    private void Update()
+    {
+        var targetPos = m_Cam.LookAt.transform.position;
+        var pos = new Vector3(.75f, 2.5f, 1.21f);
+
+        targetPos += pos;
+        m_Composer.m_TrackedObjectOffset = m_Cam.LookAt.InverseTransformPoint(targetPos);
+    }
+}
